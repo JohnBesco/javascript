42708444cbfd16425cf4536ca3b3bd57aec80a9b
John Besco <johnbesco.dev@gmail.com>
2022-11-12 22:38:20 +0000

    JavaScript Practice

--- javascript.js
+++ javascript.js
@@ -1,15 +1,3 @@
-let login = prompt('Please enter username');
-
-let message = (login == 'Employee') ? 'Hello!' :
-    (login == 'Director') ? 'Greetings!' :
-    (login == '') ? 'No Login' :
-    message = '';
-/* if (login == 'Employee') {
-    message = 'Hello';
-  } else if (login == 'Director') {
-    message = 'Greetings';
-  } else if (login == '') {
-    message = 'No login';
-  } else {
-    message = '';
-  }*/
+function displayMessage() {
+  
+}
